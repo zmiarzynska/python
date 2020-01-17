8db0d17234ddbba4edfef11ffbac03a7ad48d279
zmiarzynski <56540726+zmiarzynski@users.noreply.github.com>
2020-01-17 19:55:53 +0100

    Create 12_3.py

---
+++ z12/12_3.py
@@ -0,0 +1,35 @@
+import copy
+
+def sort_babel(L):
+    temp=len(L)
+    for i in range(len(L)):
+        for j in range(temp-1):
+            if L[j]>L[j+1]:
+                L[j],L[j + 1]=L[j+1],L[j]
+        temp-=1
+
+
+def mediana_sort(L, left, right):
+    if left<right:
+        newL= copy.copy(L)
+        sort_babel(newL)
+        mid=(left+right)//2
+        if (right - left)%2==0:
+            return newL[mid]
+        else:
+            return (newL[mid]+newL[mid+1])/2
+    return -1
+
+
+
+L=[3,2,5,3,8,2,913,5,14,8]
+print(L)
+print(mediana_sort(L,0,9))
+
+
+L=[3,2,5,3,8,2,913,4,14,8]
+print(L)
+print(mediana_sort(L,0,9))
+
+
+
