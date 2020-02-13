c9c30f6cb62ab0144810b5212666a55f18df3a06
zmiarzynski <56540726+zmiarzynski@users.noreply.github.com>
2020-02-13 13:50:37 +0100

    Create 2_10.py

---
+++ z2/2_10.py
@@ -0,0 +1,6 @@
+line="Napis \n wielowierszowy\t ala\n\n\n to dziewczynka\t"
+
+linia_w_tablicy=line.split()
+print(linia_w_tablicy)
+ilosc_wyrazow=len(linia_w_tablicy)
+print(ilosc_wyrazow) # ilosc rekordow w tablicy to ilosc slow w napisie
